ca75b2cf7afa0e12a50a2cf18e5ae83f765e2d3f
Matheus Artur <141062201+MatheusArtur0@users.noreply.github.com>
2023-10-04 21:08:41 -0300

    Create BubbleSort

---
+++ BubbleSort.py
@@ -0,0 +1,14 @@
+import random 
+
+lista_random = []
+
+numeros_unicos = rangom.sample(range(1, 1001), 100) #Gera uma lista random de 1 a 1000 delimitando 
+
+random.shuffle(numeros.unicos)
+
+lista_random.extend(numeros_unicos)
+
+print(lista_random)
+
+
+
